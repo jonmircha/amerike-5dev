96df13e0c8d783debde1a017ea93d5124a386019
Jonathan MirCha <jonmircha@gmail.com>
2024-09-17 18:44:00 -0600

    Terminando funcionalidad de aplicar descuentos y simulación de compra

--- docs/practica-5/practica-5.js
+++ docs/practica-5/practica-5.js
@@ -1,4 +1,4 @@
-// Instrucción Uno
+// Instrucción 1
 let productos = [
   { nombre: "Camiseta", precio: 15, stock: 10 },
   { nombre: "Pantalones", precio: 25, stock: 8 },
@@ -6,7 +6,7 @@ let productos = [
   { nombre: "Sombrero", precio: 10, stock: 20 },
 ];
 
-// Instrucción Dos
+// Instrucción 2
 let carrito = [];
 
 function agregarAlCarrito(productoNombre, cantidad) {
@@ -40,6 +40,27 @@ function calcularTotal() {
   return total;
 }
 
+// Instrucción 4
+function aplicarDescuento(total) {
+  if (total > 100) {
+    //Aplica un 10% de descuento
+    return total * 0.9;
+  }
+
+  return total;
+}
+
+//Instrucción 5
+function procesarCompra() {
+  console.log("Procesando compra...");
+  setTimeout(function () {
+    let total = calcularTotal();
+    total = aplicarDescuento(total);
+    console.log(`Compra completada. Total a pagar: $${total.toFixed(2)}`);
+  }, 3000);
+}
+
+// Instrucción 6
 agregarAlCarrito("Pantalones", 3);
 agregarAlCarrito("Pantalones", 4);
 agregarAlCarrito("Pantalones", 4);
@@ -49,5 +70,7 @@ agregarAlCarrito("Camiseta", 3);
 agregarAlCarrito("Pantalones", 2);
 console.log(carrito);
 
-let total = calcularTotal();
-console.log(total);
+/* let total = calcularTotal();
+total = aplicarDescuento(total);
+console.log(total); */
+procesarCompra();
