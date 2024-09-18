65a290ee80197c4c57f964b1c2aeeb58eba5b011
Jonathan MirCha <jonmircha@gmail.com>
2024-09-17 18:15:41 -0600

    Creando funcionalidad de agregar al Carrito

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Metodologías del Desarrollo para Aplicaciones Web
 
 En este repositorio encontrarán el código y recursos de la asignatura del grupo _5DEV_ de _Amerike_.
+
+[_GitHub Pages_ del repositorio](https://jonmircha.github.io/amerike-5dev/)
---
+++ docs/practica-5/practica-5.js
@@ -0,0 +1,35 @@
+// Instrucción Uno
+let productos = [
+  { nombre: "Camiseta", precio: 15, stock: 10 },
+  { nombre: "Pantalones", precio: 25, stock: 8 },
+  { nombre: "Zapatos", precio: 50, stock: 5 },
+  { nombre: "Sombrero", precio: 10, stock: 20 },
+];
+
+// Instrucción Dos
+let carrito = [];
+
+function agregarAlCarrito(productoNombre, cantidad) {
+  for (let producto of productos) {
+    if (producto.nombre === productoNombre) {
+      if (producto.stock >= cantidad) {
+        carrito.push({
+          nombre: productoNombre,
+          cantidad: cantidad,
+          precio: producto.precio,
+        });
+
+        producto.stock -= cantidad;
+        console.info(`${cantidad} ${productoNombre}(s) agregado(s) al carrito`);
+      } else {
+        console.error(`No hay suficiente stock de ${productoNombre}`);
+      }
+      return;
+    }
+  }
+  console.error(`El producto "${productoNombre}" no existe.`);
+}
+
+agregarAlCarrito("Pantalones", 3);
+agregarAlCarrito("Pantalones", 4);
+agregarAlCarrito("Pantalones", 2);
