248953eaa23627c8003112001ed138903e9fba49
Jonathan MirCha <jonmircha@gmail.com>
2024-09-24 17:39:26 -0600

    Agregando variables p6

---
+++ docs/practica-6/main.js
@@ -0,0 +1,6 @@
+const d = document;
+const $listaCarrito = d.querySelector("#lista-carrito");
+const $totalCarrito = d.querySelector("#total-carrito");
+const $btnCompra = d.querySelector("#btn-compra");
+const $mensajeCompra = d.querySelector("#mensaje-compra");
+const $carrito = d.querySelector("#carrito");
