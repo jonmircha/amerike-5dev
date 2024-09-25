adbb9840ebc4e35636b4bf1a6cebcd57b9ca012c
Jonathan MirCha <jonmircha@gmail.com>
2024-09-24 18:29:35 -0600

    Terminando función del botón de compra

--- docs/practica-6/main.js
+++ docs/practica-6/main.js
@@ -36,3 +36,12 @@ $listaCarrito.addEventListener("click", function (e) {
     $totalCarrito.innerText = (totalActual - precio).toFixed(2);
   }
 });
+
+$btnCompra.addEventListener("click", function (e) {
+  console.log($listaCarrito.children);
+  if ($listaCarrito.children.length > 0) {
+    $mensajeCompra.classList.remove("hidden");
+  } else {
+    alert("El carrito está vacío, no se puede realizar la compra.");
+  }
+});
