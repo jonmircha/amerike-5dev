6dee63eff46e69e1ce57d8d3ca31c8e08d41e165
Jonathan MirCha <jonmircha@gmail.com>
2024-10-03 17:32:21 -0600

    Agregando archivos para hacer práctica 7

---
+++ docs/practica-7/main.js
@@ -0,0 +1,17 @@
+const d = document;
+const $form = d.querySelector("#register-form");
+const $nameInput = d.querySelector("#name");
+const $nameError = d.querySelector("#name-error");
+const $emailInput = d.querySelector("#email");
+const $emailError = d.querySelector("#email-error");
+const $passwordInput = d.querySelector("#password");
+const $passwordError = d.querySelector("#password-error");
+const $confirmPasswordInput = d.querySelector("#confirm-password");
+const $confirmPasswordError = d.querySelector("#confirm-password-error");
+const $successMessage = d.querySelector("#success-message");
+const $errorsMessages = d.querySelectorAll(".error");
+
+// Función de Validación del Formulario
+function validateForm(e) {}
+
+$form.addEventListener("submit", validateForm);
