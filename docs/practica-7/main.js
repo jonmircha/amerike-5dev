9387ce217788cc5486e7a8119c38184524c1220c
Jonathan MirCha <jonmircha@gmail.com>
2024-10-03 17:49:02 -0600

    Validando que no se vayan campos vacíos el enviar el form

--- docs/practica-7/main.js
+++ docs/practica-7/main.js
@@ -12,6 +12,38 @@ const $successMessage = d.querySelector("#success-message");
 const $errorsMessages = d.querySelectorAll(".error");
 
 // Función de Validación del Formulario
-function validateForm(e) {}
+function validateForm(e) {
+  let isValid = true;
+
+  //Validar Nombre
+  if ($nameInput.value.trim() === "") {
+    $nameError.innerText = "El nombre es obligatorio";
+    isValid = false;
+  }
+
+  //Validar Email
+  if ($emailInput.value.trim() === "") {
+    $emailError.innerText = "El email es obligatorio";
+    isValid = false;
+  }
+
+  //Validar Password
+  if ($passwordInput.value.trim() === "") {
+    $passwordError.innerText = "La contraseña es obligatorio";
+    isValid = false;
+  }
+
+  //Validar Confirmar Password
+
+  //Prevenir el envío del formulario si hay errores
+  if (!isValid) {
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+    $successMessage.innerText = "Formulario enviado exitosamente.";
+    $form.reset();
+    // Aquí puedes manejar el envío real de datos a un servidor, por ejemplo, usando fetch.
+  }
+}
 
 $form.addEventListener("submit", validateForm);
