2b54d32094d766505abb5dc4133975cde2b9c5b9
Jonathan MirCha <jonmircha@gmail.com>
2025-03-11 16:01:06 -0600

    Práctica Intro JS

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,23 @@
 //alert("Hola Mundo");
+console.log("Hola Mundo JavaScript");
+
+let nombre = "Jon";
+let edad = 40;
+
+//La Interpolación es el proceso de insertar variables dentro de una cadena de texto estática
+console.log(`Hola mi nombre es ${nombre}, y tengo ${edad} años`);
+
+let cadena = "Hola, mundo";
+let numero = 42;
+let booleano = true;
+let lista = [1, 2, 3, 4];
+let objeto = { nombre: "Ana", edad: 25 };
+
+console.log(cadena);
+console.log(numero);
+console.log(booleano);
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.length);
+console.log(objeto);
+console.log(objeto.nombre);
